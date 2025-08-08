96622b9c066d39dd3954cd70eaf256257b2580f5
Irena Chlumsky <chlumskyirena25@gamil.com>
2025-08-08 12:38:06 +0200

    Komponenten Erstellen und Einbuinden

--- Semesterarbeit/src/app/app.ts
+++ Semesterarbeit/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header} from './header/header';
+import { Footer } from './footer/footer';
+import { Nav } from './nav/nav';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
