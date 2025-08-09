aa28af31ec85531eda01d65f1040e79517621539
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-09 10:32:26 +0200

    Vorläufiges GrundLayout

--- Semesterarbeit/src/app/app.routes.ts
+++ Semesterarbeit/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Form as Form } from './form/form';
+import { Table as Table } from './table/table';
 
-export const routes: Routes = [];
+export const routes: Routes = [ { path: 'create', component: Form },  // Formular
+  { path: 'read',   component: Table }, // Tabelle
+  { path: '', pathMatch: 'full', redirectTo: 'read' }];
