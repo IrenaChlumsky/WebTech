f8f42744b3288e1df7dc27b77299348146655867
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-10 10:59:41 +0200

    Styling verbessert mit Hilfe von Bootstrap

--- Semesterarbeit/src/app/app.routes.ts
+++ Semesterarbeit/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home as Home } from './home/home';
 import { Form as Form } from './form/form';
 import { Table as Table } from './table/table';
 
-export const routes: Routes = [ { path: 'create', component: Form },  // Formular
-  { path: 'read',   component: Table }, // Tabelle
-  { path: '', pathMatch: 'full', redirectTo: 'read' }];
+
+export const routes: Routes = [ { path: 'create', component: Form }, 
+  { path: 'read',   component: Table }, 
+  { path: '', pathMatch: 'full', redirectTo: 'read' },
+  {path: 'home', component: Home }, 
+ 
+];
