0221591d8ea192eedab4d903ff333a4e69e881a1
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-12 10:51:04 +0200

    Tabelle mit service + eventbiding

--- Semesterarbeit/src/app/table/table.ts
+++ Semesterarbeit/src/app/table/table.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DataService } from '../shared/data';
+import { Pokemon } from '../shared/pokemon';
 
 @Component({
   selector: 'app-table',
@@ -6,6 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './table.html',
   styleUrl: './table.css'
 })
-export class Table {
+export class Table implements OnInit {
+private data = inject(DataService);
+
+  pokemons: Pokemon[] = [];
+  loading = true;
+  error = '';
+
+  async ngOnInit() {
+    try {
+      this.pokemons = await this.data.getAll();
+    } catch (e) {
+      this.error = (e as Error).message ?? 'Fehler beim Laden';
+    } finally {
+      this.loading = false;
+    }
+  }
+onEdit(p: Pokemon) {
+  console.log('edit', p);
+}
+
+onRemove(p: Pokemon) {
+  console.log('remove', p);
+}
+
 
 }
+
