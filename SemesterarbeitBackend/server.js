b839d243ad9479faae0d32e8117ef7cfdb22ee89
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-16 10:00:28 +0200

    anbibndung an Datenbank mit Mongoose

--- SemesterarbeitBackend/server.js
+++ SemesterarbeitBackend/server.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const routes = require('./routes');
 
-
+const express   = require('express');
+const routes    = require('./routes');
+const mongoose  = require('mongoose');
+require('dotenv').config();           
 
 const app = express();
+
 app.use(express.json());
 app.use('/', routes);
 
-const PORT = 3000;
+
+mongoose.connect(process.env.MONGO_URI, { dbName: 'pokemon' });
+
+const db = mongoose.connection;
+db.on('error', (err) => {
+  console.error('MongoDB-Fehler:', err);
+});
+db.once('open', () => {
+  console.log('connected to DB');
+});
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
   if (err) console.error(err);
   else console.log(`Server started and listening on port ${PORT} ...`);
