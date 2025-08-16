292143fccf3f4184c97d2ae31447bc9e535da58b
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-16 09:18:38 +0200

    anpassung von routes

--- SemesterarbeitBackend/server.js
+++ SemesterarbeitBackend/server.js
@@ -1,28 +1,14 @@
-import 'dotenv/config';
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express');
+const routes = require('./routes');
+
+
 
 const app = express();
 app.use(express.json());
+app.use('/', routes);
 
-const { PORT = 3000, MONGO_URI } = process.env;
-
-async function start() {
-  try {
-    await mongoose.connect(MONGO_URI);
-    console.log('✅ MongoDB verbunden');
-
-    app.get('/health', (req, res) => {
-      res.json({ ok: true });
-    });
-
-    app.listen(PORT, () => {
-      console.log(`✅ API läuft auf http://localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error('❌ DB-Verbindungsfehler:', err.message);
-    process.exit(1);
-  }
-}
-
-start();
+const PORT = 3000;
+app.listen(PORT, (err) => {
+  if (err) console.error(err);
+  else console.log(`Server started and listening on port ${PORT} ...`);
+});
