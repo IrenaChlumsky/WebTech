e18bb24ac1feadb99fec2076455de65f93478395
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-17 12:27:51 +0200

    POST implementierung

--- SemesterarbeitBackend/routes.js
+++ SemesterarbeitBackend/routes.js
@@ -8,5 +8,17 @@ router.get('/pokemon', async(req, res) => {
     res.send(allPokemon);
  
 });
+router.post('/pokemon', async(req, res) => {
+    const newPokemon = new Pokemon({
+        name: req.body.name,
+        level: req.body.level,
+        type1: req.body.type1,
+        type2: req.body.type2,
+        attacks: req.body.attacks
+    });
+    await newPokemon.save();
+    res.send(newPokemon);
+});
+
 
 module.exports = router;
