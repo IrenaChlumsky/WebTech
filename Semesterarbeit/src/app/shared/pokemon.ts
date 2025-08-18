93e1c34042bf7ded9ea75871345c2f56b3e279a7
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-18 13:25:16 +0200

    Pokemon Interface angelegt

--- Semesterarbeit/src/app/shared/pokemon.ts
+++ Semesterarbeit/src/app/shared/pokemon.ts
@@ -1,12 +1,12 @@
 export type PokemonType =
   | 'Normal' | 'Feuer' | 'Wasser' | 'Pflanze' | 'Elektro' | 'Eis'
   | 'Kampf'  | 'Gift'  | 'Boden'  | 'Flug'    | 'Psycho'  | 'Käfer'
-  | 'Gestein'| 'Geist' | 'Drache' | 'Unlicht' | 'Stahl'   | 'Fee';  //hilfe von chat gpt
+  | 'Gestein'| 'Geist' | 'Drache' | 'Unlicht' | 'Stahl'   | 'Fee';  
 
 export interface Pokemon {
   name: string;
-  level: number;      // 1–100
-  type1: PokemonType; // Pflicht
-  type2?: PokemonType;// optional
-  attacks: string[];  // z. B. ["Donnerblitz","Ruckzuckhieb"]
+  level: number;     
+  type1: PokemonType; 
+  type2?: PokemonType;
+  attacks: string[]; 
 }
