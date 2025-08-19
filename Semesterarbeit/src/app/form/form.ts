93f2218906bf72cdc8436383a61bb5d79e79479f
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-19 16:05:46 +0200

    Formular Validierung implementiert

--- Semesterarbeit/src/app/form/form.ts
+++ Semesterarbeit/src/app/form/form.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form.html',
   styleUrl: './form.css'
 })
 export class Form {
+  form = new FormGroup({
+    name:   new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+    level:  new FormControl<number | null>(null, [Validators.required, Validators.min(1), Validators.max(100)]),
+    type1:  new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+    type2:  new FormControl<string>(''),
+    attacks:new FormControl<string>(''),
+  });
 
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Pokémon gespeichert:', this.form.value);
+      // hier später: Backend-Call
+    } else {
+    
+      this.form.markAllAsTouched();
+      console.log('Formular ist ungültig');
+    }
+  }
 }
