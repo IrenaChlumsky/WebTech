5ed1eb41f3c8aacc15c41a0e60f992bf8f36378a
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-20 14:07:02 +0200

    on submit angebunden

--- Semesterarbeit/src/app/form/form.ts
+++ Semesterarbeit/src/app/form/form.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { BackendService } from '../shared/backend';
+import { Pokemon } from '../shared/pokemon';
+
+
+
 
 @Component({
   selector: 'app-form',
@@ -9,6 +14,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './form.html',
   styleUrl: './form.css'
 })
+
 export class Form {
   form = new FormGroup({
     name:   new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
@@ -17,15 +23,46 @@ export class Form {
     type2:  new FormControl<string>(''),
     attacks:new FormControl<string>(''),
   });
+  saving = false;
+  successMsg = '';
+  errorMsg = '';
 
-  onSubmit() {
-    if (this.form.valid) {
-      console.log('Pokémon gespeichert:', this.form.value);
-      // hier später: Backend-Call
-    } else {
-    
+  constructor(private backend: BackendService) {}
+
+  async onSubmit(): Promise<void> {
+    this.successMsg = '';
+    this.errorMsg = '';
+    if (this.form.invalid) {
       this.form.markAllAsTouched();
       console.log('Formular ist ungültig');
+      return;
     }
+
+     this.saving = true;
+    const dto = {
+  ...(this.form.getRawValue() as any),
+  level: Number(this.form.value.level ?? 0),
+  attacks: (this.form.value.attacks ?? '')
+            .split(',')
+            .map((s: string) => s.trim())
+            .filter(Boolean)
+} as Pokemon;
+    
+      try {
+      await this.backend.create(dto);       // ⬅️ HIER passiert das Speichern
+      this.successMsg = 'Gespeichert.';
+      this.form.reset();
+    } catch (e: any) {
+      console.error(e);
+      this.errorMsg = 'Speichern fehlgeschlagen.';
+    } finally {
+      this.saving = false;
+    }
+  }
+
+  onCancel(): void {
+    this.form.reset();
+    this.successMsg = '';
+    this.errorMsg = '';
   }
 }
