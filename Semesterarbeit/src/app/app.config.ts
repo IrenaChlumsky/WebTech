52dd28868625d7ca8d484c1b015573e06a1b16fd
Irena Chlumsky <223063728+IrenaChlumsky@users.noreply.github.com>
2025-08-26 10:12:57 +0200

    Löschen von unnotigen imports

--- Semesterarbeit/src/app/app.config.ts
+++ Semesterarbeit/src/app/app.config.ts
@@ -1,19 +1,12 @@
 
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-  provideBrowserGlobalErrorListeners,
- 
-} from '@angular/core';
+import { ApplicationConfig,} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    
   ],
 };
--- Semesterarbeit/src/app/header/header.ts
+++ Semesterarbeit/src/app/header/header.ts
@@ -8,5 +8,4 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
-
 }
